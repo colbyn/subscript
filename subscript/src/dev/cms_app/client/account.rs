ecf8fe8f0846626e116c9baecaade0bfa260262d
Colbyn Wadman <hello@colbyn.com>
2019-06-25 16:44:17 -0600

    Account page updates.

--- subscript/src/dev/cms_app/client/account.rs
+++ subscript/src/dev/cms_app/client/account.rs
@@ -20,6 +20,7 @@ use crate::program_sys::instances::Component;
 use crate::program_sys::spec::*;
 use crate::program_sys::{self, Program};
 
+// use crate::dev::cms_app::client::AppSpec;
 use crate::dev::cms_app::client::data::*;
 use crate::dev::cms_app::client::ui_utils::{self, text_theme};
 use crate::dev::cms_app::client::account::billing::BillingSpec;
@@ -40,6 +41,7 @@ pub struct AccountSpec {
 
 pub enum Msg {
     NoOp,
+    UrlRequest(Page)
 }
 
 #[derive(Default)]
@@ -73,33 +75,18 @@ impl Spec for AccountSpec {
     fn update(&self, model: &mut Model, msg: Msg, sh: &mut Shell<Self>) {
         match msg {
             Msg::NoOp => {}
+            Msg::UrlRequest(page) => {
+                // sh.message::<AppSpec>(UrlRequest(page));
+            }
         }
     }
-    fn view(&self, model: &Model) -> View<Msg> {
-        match &self.page {
-            AccountPage::Password => v1!{
-                Component::singleton(PasswordSpec{
-                    session: self.session.clone(),
-                });
-            },
-            AccountPage::Email => v1!{
-                Component::singleton(EmailSpec{
-                    session: self.session.clone(),
-                });
-            },
-            AccountPage::Users(users_page) => v1!{
-                Component::singleton(UsersSpec{
-                    session: self.session.clone(),
-                    page: users_page.clone(),
-                });
-            },
-            AccountPage::Billing => v1!{
-                Component::singleton(BillingSpec{
-                    session: self.session.clone(),
-                });
-            },
-        }
-    }
+    fn view(&self, model: &Model) -> View<Msg> {v1!{
+        display: "grid";
+        grid_template_columns: "max-content 1fr";
+        grid_column_gap: "20px";
+        navigation(self, model);
+        page(self, model);
+    }}
 }
 
 
@@ -107,3 +94,75 @@ impl Spec for AccountSpec {
 // VIEW HELPERS
 ///////////////////////////////////////////////////////////////////////////////
 
+fn navigation(account: &AccountSpec, model: &Model) -> View<Msg> {
+    let link = |page: AccountPage, text: &str| v1!{
+        li !{
+            display: "block";
+            // {
+            //     if account.page == page {v1!{
+            //         border_bottom: "3px solid #0089ff";
+            //     }}
+            //     else {v1!{}}
+            // };
+            a !{
+                user_select: "none";
+                display: "block";
+                font_weight: "500";
+                padding: "8px";
+                event.click[page] => move || {
+                    Msg::UrlRequest(Page::Account(page))
+                };
+                text;
+            };
+        };
+    };
+    v1!{
+        aside !{
+            section !{
+                h3 !{
+                    text_theme();
+                    "Personal Settings";
+                };
+                ul !{
+                    margin: "0";
+                    link(AccountPage::Password, "Password");
+                };
+            };
+            section !{
+                h3 !{
+                    text_theme();
+                    "Organization Settings";
+                };
+                ul !{
+                    margin: "0";
+                };
+            };
+        };
+    }
+}
+
+fn page(account: &AccountSpec, model: &Model) -> View<Msg> {
+    match &account.page {
+        AccountPage::Password => v1!{
+            Component::singleton(PasswordSpec{
+                session: account.session.clone(),
+            });
+        },
+        AccountPage::Email => v1!{
+            Component::singleton(EmailSpec{
+                session: account.session.clone(),
+            });
+        },
+        AccountPage::Users(users_page) => v1!{
+            Component::singleton(UsersSpec{
+                session: account.session.clone(),
+                page: users_page.clone(),
+            });
+        },
+        AccountPage::Billing => v1!{
+            Component::singleton(BillingSpec{
+                session: account.session.clone(),
+            });
+        },
+    }
+}
