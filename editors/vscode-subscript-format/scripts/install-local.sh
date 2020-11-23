727cc515c640de0c06fb3a74295392fee1eb9a26
Colbyn Wadman <hello@colbyn.com>
2020-11-23 16:02:49 -0700

    update local install script

--- editors/vscode-subscript-format/scripts/install-local.sh
+++ editors/vscode-subscript-format/scripts/install-local.sh
@@ -1,1 +1,4 @@
-cp -r $(pwd) ~/.vscode/extensions/subscript-format
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT_PLUGIN_DIR="$SCRIPTS_DIR/../"
+
+cp -r $ROOT_PLUGIN_DIR ~/.vscode/extensions/subscript-format
