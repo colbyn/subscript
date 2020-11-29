712d832c2d31547e986c923a28fede7df3ceea6f
Colbyn Wadman <hello@colbyn.com>
2020-11-29 12:13:07 -0700

    allow for passing in cargo flags such as for building in release mode

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,7 +1,19 @@
 set -e
 
+CARGO_FLAGS=""
+
+if [ -z "$1" ]
+then
+    # NOT DEFINED
+    CARGO_FLAGS+=""
+else
+    # OTHERWISE - DEFINED
+    CARGO_FLAGS+="$1"
+fi
+
+
 rm -rf school-notes/output
-cargo run -- serve \
+cargo run $CARGO_FLAGS -- serve \
     --root=school-notes \
     --input 'school-notes/pages/**/*.html' \
     --output=school-notes/output \
